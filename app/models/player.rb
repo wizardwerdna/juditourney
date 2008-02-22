84e599e51703a67f8aee065df6cbfb4ccfc25fb0
werdna <werdna@cb19f998-2f92-47fb-9dfd-6d91e90c5b80>
2008-02-22 22:17:04 +0000

    updated player show action to reflect legue status, improved model to better reflect model associations and load leagues more efficiently

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,14 @@
 class Player < ActiveRecord::Base
+  has_many :entries
+  has_many :tournaments, :through => :entries
+  has_many :league_tournaments, :through => :entries, :source => :tournament, :include => :league
+  
   validates_uniqueness_of :first, :scope => :last
   validates_presence_of :last
+  
+  def leagues
+    league_tournaments.collect{|tournament| tournament.league}.uniq
+  end
   
   def full_name
     first + " " + last    
