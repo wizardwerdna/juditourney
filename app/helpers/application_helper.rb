4661b16e241060c54d475dc3be902b7815f60dbb
werdna <werdna@cb19f998-2f92-47fb-9dfd-6d91e90c5b80>
2008-03-18 18:19:28 +0000

    fixed layout to work in ie7, need to check ie6 compatibility

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -74,10 +74,4 @@ module ApplicationHelper
   def tournament_entry_crumb(tournament, entry)
     {:name => "Entry", :url => tournament_entries_url(@tournament)}
   end 
-  
-  def dom_id_of_tile_to_insert_after_for(resource_finder, entry)
-    record = resource_finder.find(:first, :conditions => ['result < ?', entry.result], :order => 'result desc')
-    @after_id = (record && dom_id(record)) || 'sentry'
-  end
-  
 end
