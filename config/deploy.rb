d32b0e8e1beea6983d0e41181bb91afd3278aca6
Andrew Greenberg <wizardwerdna@gmail.com>
2008-05-20 03:10:18 -0400

    change deployment to railsplayground

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,22 @@
-set :application, "juditourney"
-default_run_options[:pty] = true
+set :user, 'werdna'
+set :server, 'lawhacker.com'
+set :application, "juditourney" 
 set :repository,  "git@github.com:wizardwerdna/juditourney.git"
 set :scm, "git"
-set :git, "/usr/local/git/bin/git"
+set :git, "/usr/local/bin/git"
 set :user, "werdna"
 set :branch, "origin/master"
 set :deploy_via, :remote_cache
-role :app, "pokerwiz.gotdns.com"
-role :web, "pokerwiz.gotdns.com"
-role :db,  "pokerwiz.gotdns.com", :primary => true
-set :use_sudo, false
+role :web, "lawhacker.com"
+role :app, "lawhacker.com"
+role :db,  "lawhacker.com", :primary => true
+
+set :deploy_to, "/home/werdna/juditourney" 
+
+task :restart, :roles => :app do
+  touch "/home/werdna/juditourney/current/tmp/foo.txt"
+end
+
+task :after_update_code, :roles => [:web, :db, :app] do
+  run "chmod 755 /home/werdna/juditourney/current/public -R" 
+end
